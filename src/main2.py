b4e8d69b9ce1471fd45690fbed73b4ceaffc2781
Menings <menings.git@gmail.com>
2025-03-29 08:49:28 +0100

    vocs are now translated and checked if new. Next step is to make anki flashcards

--- src/main2.py
+++ src/main2.py
@@ -1,35 +1,19 @@
-import scrapping
+import scrapping, voc_helper
 
 import screenshots, text_recognition, index_cards, anki
 import os, glob, googletrans
 
-voc_translated = {}
-vocabulary = set()
 
 all_notes = scrapping.scrap_notes()
-vocabulary = scrapping.sort_notes(all_notes, "orange")
+new_voc_all= set(scrapping.sort_notes(all_notes, "orange"))
 
+total_voc = voc_helper.vocabulary()
+new_voc = total_voc.check_new_words(new_voc_all)
 
-# translate into german and make a dictionary where the keys are the english words and the values the german translation
-translator = googletrans.Translator()
-for word in vocabulary:
-    translation = translator.translate(word, dest='de').text
-    translation = translation.replace('ß', 'sz')
-    translation = translation.replace('Ü', 'Ue')
-    translation = translation.replace('Ä', 'Ae')
-    translation = translation.replace('Ö', 'Oe')
-    translation = translation.replace('ü', 'ue')
-    translation = translation.replace('ä', 'ae')
-    translation = translation.replace('ö', 'oe')
+new_voc_translated = voc_helper.tanslate_vocabulary(new_voc)
 
-    voc_translated[word] = translation
-
-# create index_cards
-# index_cards.open_cartigo()
-# for key in voc_translated.keys():
-#     index_cards.add_card(word_en=key, word_de=voc_translated[key])
-# screenshots.close_edge()
+total_voc.save_vocabulary(new_voc)
 
 # save as a txt
-anki.append_voc_to_data(voc_translated)
-print(voc_translated)
+#anki.append_voc_to_data(voc_translated)
+#print(voc_translated)
--- src/voc_helper.py
+++ src/voc_helper.py
@@ -19,18 +19,25 @@ def tanslate_vocabulary(vocabulary):
 
 class vocabulary:
     def __init__(self):
+        self.path = r"C:\Users\Benja\Code\Python\vocabulary_automatisation\src\voclist\vocabulary2.txt"
         self.all_vocs = self.load_vocabulary()
     
     def load_vocabulary(self):
-        path = r"C:\Users\Benja\Code\Python\vocabulary_automatisation\src\voclist\vocabulary2.txt"
         all_vocs = set()
-        with open(path, 'r') as file:
+        with open(self.path, 'r') as file:
             for line in file:
                 line = line.strip('\n')
                 all_vocs.add(line)
         return all_vocs
 
     def check_new_words(self, voc_new):
-        return voc_new-self.all_vocs 
+        return voc_new-self.all_vocs
+    
+    def save_vocabulary(self, new_voc):
+        self.all_vocs += new_voc
+        with open(self.path, 'w') as file:
+            for word in self.all_vocs:
+                file.write(word+'\n')
+            file.close()
 
 
